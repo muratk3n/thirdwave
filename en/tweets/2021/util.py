75fd089093b3fe3d17344443a46cd826e0766bb8
MK <me@yomama.com>
2021-02-16 08:56:27 +0300

    -

---
+++ en/tweets/2021/util.py
@@ -0,0 +1,27 @@
+import requests, time, datetime
+
+def get_eq1():
+    today = datetime.datetime.now()
+    days = 90
+    start = today - datetime.timedelta(days=days)
+
+    req = 'https://earthquake.usgs.gov/fdsnws'
+    req+='/event/1/query.geojson?starttime=%s&endtime=%s'
+    req+='&minmagnitude=4.5&orderby=time&limit=1000'
+    req = req % (start.isoformat(), today.isoformat())
+    qr = requests.get(req).json()
+    res = []
+    for i in range(len(qr['features'])):
+        lat = qr['features'][i]['geometry']['coordinates'][1]
+        lon = qr['features'][i]['geometry']['coordinates'][0]
+        rad = qr['features'][i]['geometry']['coordinates'][2]
+        d = datetime.datetime.fromtimestamp(qr['features'][i]['properties']['time']/1000.0)
+        s = np.float(qr['features'][i]['properties']['mag'])
+        diff = (d-start).days
+        res.append([d,s,lat,lon,rad,diff])
+
+    import pandas as pd
+    df = pd.DataFrame(res).sort_values(by=0)
+    df = df.set_index(0)
+    df.columns = ['mag','lat','lon','rad','ago']
+    return df
